991f1ae04789b4c03b373964188711cc5edf2e43
cckinwest <kensechan@gmail.com>
2023-08-26 17:57:46 +0100

    active button is highlighted now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "../components/styles/NavBar.css";
 
 function Navbar(props) {
+  //const [activeBtn, setActiveBtn] = useState("About");
+
+  //const pages = ["About", "Contact", "Portfolio", "Resume"];
+
   const handleClick = (page) => {
     props.setActive(page);
+  };
+
+  const status = (btnName) => {
+    if (props.active === btnName) {
+      return "active";
+    }
+
+    return "inActive";
   };
 
   return (
     <div>
       <h1 className="headerTitle">Chi Kin's page</h1>
-      <ul className="btnContainer">
+      <ul
+        className="btnContainer"
+        style={{ display: "flex", listStyleType: "none" }}
+      >
         <li>
-          <a href={`#about`} key="About" onClick={() => handleClick("About")}>
+          <a
+            href={`#about`}
+            key="About"
+            onClick={() => handleClick("About")}
+            className={status("About")}
+          >
             About
           </a>
         </li>
@@ -19,6 +40,7 @@ function Navbar(props) {
             href={`#contact`}
             key="Contact"
             onClick={() => handleClick("Contact")}
+            className={status("Contact")}
           >
             Contact
           </a>
@@ -28,6 +50,7 @@ function Navbar(props) {
             href={`#portfolio`}
             key="portfolio"
             onClick={() => handleClick("Portfolio")}
+            className={status("Portfolio")}
           >
             Portfolio
           </a>
@@ -37,6 +60,7 @@ function Navbar(props) {
             href={`#resume`}
             key="resume"
             onClick={() => handleClick("Resume")}
+            className={status("Resume")}
           >
             Resume
           </a>
